4bc490ee0a38fb3bb4c142fa8c421937038c19db
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-07-28 04:13:48 +0300

    added a public error handling logic

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,1 +1,40 @@
-// 
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private httpClient:HttpClient) { }
+
+   //for github profile
+  public getProfile(searchQuery: any):Observable<any>{
+    let dataURL = `https://api.github.com/users/${searchQuery}`
+    return this.httpClient.get<any>(dataURL)
+  }
+  public getrepos(searchQuery: any):Observable<any[]>{
+    let dataURL = `https://api.github.com/users/${searchQuery}/repos`
+    return this.httpClient.get<any[]>(dataURL)
+  }
+
+  public handleErrors(error:HttpErrorResponse){
+    let errorMessage:string;
+    if(error.error instanceof ErrorEvent){
+      errorMessage = `MESSAGE : ${error.error.message}`;
+    }
+    else{
+      errorMessage = `STATUS : ${error.status} MESSAGE :${error.message}`
+    }
+    return throwError(errorMessage);
+  }
+}
+function retry(count: any, arg1: number): import("rxjs").OperatorFunction<any[], any[]> {
+  throw new Error('Function not implemented.');
+}
+
+function count(count: any, arg1: number): import("rxjs").OperatorFunction<any[], any[]> {
+  throw new Error('Function not implemented.');
+}
+
